699d362347955bfe0d45a6639aef4d7171ebf8e1
shijiaa <seveny1997@hotmail.com>
2019-04-22 09:56:07 +0200

    Update helpers.py

--- lipnet/lipreading/helpers.py
+++ lipnet/lipreading/helpers.py
@@ -1,4 +1,4 @@
-# char -> int
+# "char" -> [int]
 def text_to_labels(text):
     ret = []
     for char in text:
@@ -8,7 +8,7 @@ def text_to_labels(text):
             ret.append(26)
     return ret
 
-# int -> char
+# [int] -> "char"
 def labels_to_text(labels):
     # 26 is space, 27 is CTC blank char
     text = ''
